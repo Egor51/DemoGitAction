13ad52201fe3b37ef3409aa9ee033f73553af660
Egor51 <kubrakov_egor87@mail.ru>
2022-12-27 13:20:06 +0300

    initial commit

---
+++ src/test/java/com/example/testgitaction/TestGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
